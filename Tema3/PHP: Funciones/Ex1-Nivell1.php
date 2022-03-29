7251c09f70fcf607a3f62b83ba7e7001f759f88d
Roger Gonzalez <96357150+RogerGQ@users.noreply.github.com>
2022-03-29 12:32:21 +0200

    Update Ex1-Nivell1.php

--- Tema3/PHP: Funciones/Ex1-Nivell1.php
+++ Tema3/PHP: Funciones/Ex1-Nivell1.php
@@ -1,1 +1,13 @@
+<?php
+function numero($numero_1) {
+        if ($numero_1 % 2 > 0){
+            echo "El numero es inpar";
+            echo "<br/>";
+        } else {
+            echo "El numero es par";
+            echo "<br/>";
+        }
+    }
+numero(2);
+?>
 
