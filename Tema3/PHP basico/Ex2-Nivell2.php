86d30c2e668adbee57d7bb5a5ac7ec681e114083
Roger Gonzalez <96357150+RogerGQ@users.noreply.github.com>
2022-03-29 12:02:14 +0200

    Create Ex2-Nivell2.php

---
+++ Tema3/PHP basico/Ex2-Nivell2.php
@@ -0,0 +1,19 @@
+<?php
+
+$palabra = "Hello, World";
+$palabra_mayus = strtoupper($palabra);
+$palabra2 = "Aquest es el curs de PHP";
+
+echo ($palabra);
+echo ("<br>");
+echo ($palabra_mayus);
+echo ("<br>");
+echo strlen ($palabra);
+echo ("<br>");
+echo strrev ($palabra);
+echo ("<br>");
+echo ($palabra2);
+echo ("<br>");
+echo ($palabra . $palabra2);
+
+?>
