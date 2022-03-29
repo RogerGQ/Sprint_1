661f17073457ece1329539fab4c0b6abe9b3fe79
Roger Gonzalez <96357150+RogerGQ@users.noreply.github.com>
2022-03-29 12:01:44 +0200

    Update Ex1-Nivell1.php

--- Tema3/PHP basico/Ex1-Nivell1.php
+++ Tema3/PHP basico/Ex1-Nivell1.php
@@ -1,1 +1,18 @@
+<?php
+
+$integer = 50;
+$double = 2.5;
+$string = "Hola";
+$boolean = False;
+
+echo ($integer);
+echo ("<br>");
+echo ($double);
+echo ("<br>");
+echo ($string);
+echo ("<br>");
+echo ($boolean);
+
+?>
+
 
