f7afca69377975a14dd72225531de9e306f70aa7
Roger Gonzalez <96357150+RogerGQ@users.noreply.github.com>
2022-03-31 12:04:04 +0200

    Impresion en pantalla de nombre y sueldo

--- Tema3/PHP: Objetos/Ex1-Nivell..php
+++ Tema3/PHP: Objetos/Ex1-Nivell..php
@@ -1,1 +1,27 @@
+<?php
+// Definicion de classe
+class Employee {
+      // Atributos de la classe
+      protected $nombre;
+      protected $sueldo;
+      // Metodos de la classe
+      public function initialize($nom,$sou){
+          $this -> nombre = $nom;
+          $this -> sueldo = $sou;
+      }
+      public function print(){
+          return $this -> nombre;
+          return $this -> sueldo;
+      }
+      public function printNombre(){
+        return $this -> nombre;
+      }
+      public function printSueldo(){
+          return $this -> sueldo;
+      }
+}
+$persona = new Employee();
+$persona -> initialize ('Juan', 2500);
+echo $persona -> printNombre().' - '.$persona -> printSueldo();
+?>
 
