63a63a1daf3b6f638021e947f737964fd3dfad96
Roger Gonzalez <96357150+RogerGQ@users.noreply.github.com>
2022-04-01 09:08:24 +0200

    V.1.0 Final

--- Tema3/PHP: Objetos/Ex1-Nivell..php
+++ Tema3/PHP: Objetos/Ex1-Nivell..php
@@ -1,24 +1,28 @@
 <?php
 // Definicion de classe
 class Employee {
-      // Atributos de la classe
-      protected $nombre;
-      protected $sueldo;
-      // Metodos de la classe
-      public function initialize($nom,$sou){
-          $this -> nombre = $nom;
-          $this -> sueldo = $sou;
-      }
-      public function printNombre(){
-          return $this -> nombre;
-      }
-      public function printSueldo(){
-          return $this -> sueldo;
-      }
-     
+    // Atributos de la classe
+    protected $nombre;
+    protected $sueldo;
+    // Metodos de la classe
+    public function initialize($nom,$sou) {
+        $this -> nombre = $nom;
+        $this -> sueldo = $sou;
+    }
+    public function printNombre() {
+        return $this -> nombre;
+    }
+    public function printSueldo() {
+        return $this -> sueldo;
+    }
 }
-$persona = new Employee();
-$persona -> initialize ('Juan', rand(0,10000)); 
-echo $persona -> printNombre().' - '.$persona -> printSueldo().'<br>';
-?>
 
+$persona = new Employee();
+$persona -> initialize ('Juan', $sou = rand(0,10000));
+echo $persona -> printNombre().' - '.$persona -> printSueldo().'<br>';
+if ($sou >= 6000) {
+    echo "Ha de pagar impuestos";
+} else {
+    echo "No ha de pagar impuestos";
+}
+?>
